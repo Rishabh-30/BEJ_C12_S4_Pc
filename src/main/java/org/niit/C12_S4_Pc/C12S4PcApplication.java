49d60a100bbaa6f9bd7b0a0159d5f1dac3147545
Rishabh Pathak <rishabh.pathak30@hotmail.com>
2022-11-23 22:07:02 +0530

    Completed

---
+++ src/main/java/org/niit/C12_S4_Pc/C12S4PcApplication.java
@@ -0,0 +1,14 @@
+package org.niit.C12_S4_Pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C12S4PcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C12S4PcApplication.class, args);
+		System.out.println("GOOD DAY TO YOU");
+	}
+
+}
---
+++ src/test/java/org/niit/C12_S4_Pc/C12S4PcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niit.C12_S4_Pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C12S4PcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
